5ddffbf49c42a8f5d9cbd023a81bcf805dc751dc
LeCanardNoir <dev.lecanardnoir@gmail.com>
2023-09-27 16:51:24 -0400

    INIT: Add file prject

--- main.cpp
+++ main.cpp
@@ -1,12 +1,16 @@
 ﻿// Vector_Deque.cpp : définit le point d'entrée de l'application.
 //
 
-#include "main.h"
+#include <iostream>
+#include "vector.h"
+#include "deque.h"
 
 using namespace std;
 
 int main()
 {
 	cout << "Hello CMake." << endl;
+	int t;
+	cin >> t;
 	return 0;
 }
